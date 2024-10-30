44a6f88f815ce7c66f08f3a62bccc678e66f2ccf
billygrahan <billygrahan@alu.ufc.br>
2024-10-30 00:00:36 -0300

    não sei porque não foi a questão 6(mas agora foi)

---
+++ Questao_6/ListaIntervalo.cs
@@ -0,0 +1,36 @@
+﻿using Questao_5;
+namespace Questao_6;
+
+public class ListaIntervalo
+{
+    public List<Intervalo> intervalos = new List<Intervalo>();
+
+    /*public ListaIntervalo(List<Intervalo> intervalos)
+    {
+        this.intervalos = intervalos;
+    }*/
+
+    public List<Intervalo> Intervalos
+    {
+        get
+        {
+            return intervalos.OrderBy(i => i.DataInicial).ToList();
+        }
+    }
+
+    public bool Add(Intervalo intervalo)
+    {
+        foreach (Intervalo i in intervalos)
+        {
+         if (i.TemIntersecao(intervalo))
+            {
+                Console.WriteLine($"\nintervalo ({intervalo.DataInicial},{intervalo.DataFinal} não adicionado por fazer interceção com evento ({i.DataInicial},{i.DataInicial})\n");
+                return false;
+            }
+        }
+        
+        intervalos.Add(intervalo);
+        Console.WriteLine($"\nintervalo {intervalo.DataInicial},{intervalo.DataFinal} Adicionado\n");
+        return true;
+    }
+}
---
+++ Questao_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using Questao_5;
+using Questao_6;
+
+Intervalo evento1 = new Intervalo(new DateTime(2026, 10, 19, 00, 00, 00), new DateTime(2026, 10, 21, 23, 59, 59));
+Intervalo evento2 = new Intervalo(new DateTime(2024, 10, 19, 00, 00, 00), new DateTime(2024, 10, 21, 23, 59, 59));
+Intervalo evento3 = new Intervalo(new DateTime(2024, 10, 24, 00, 00, 00), new DateTime(2024, 10, 26, 23, 59, 59));
+Intervalo evento4 = new Intervalo(new DateTime(2024, 10, 21, 00, 00, 00), new DateTime(2024, 10, 24, 23, 59, 59));
+
+ListaIntervalo eventos =  new ListaIntervalo();
+
+eventos.Add(evento1);
+eventos.Add(evento2);
+eventos.Add(evento3);
+eventos.Add(evento4);
+
+foreach (Intervalo evento in eventos.intervalos) 
+    Console.WriteLine($"(inicio : {evento.DataInicial} , Fim : {evento.DataFinal})");
+
+foreach (Intervalo evento in eventos.Intervalos) 
+    Console.WriteLine($"(inicio : {evento.DataInicial} , Fim : {evento.DataFinal})");
